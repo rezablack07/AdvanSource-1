42dcb9f9365afd6f3a7c1ba305e7be5bd3710bce
taha560 <taha102050@gmail.com>
2016-04-22 06:26:29 -0700

    Update teleadvan.lua

--- bot/teleadvan.lua
+++ bot/teleadvan.lua
@@ -231,7 +231,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {111984481},--Sudo users
+    sudo_users = {189308877},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
